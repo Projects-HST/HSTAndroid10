a94f54b7778cc0173a79b369246adf3fa7e27bdd
Nandhakumar <nandha@happysanztech.com>
2020-12-23 13:15:16 +0530

    Initial commit

---
+++ app/src/main/java/com/hst/osa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hst.osa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
