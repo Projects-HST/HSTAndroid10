abe155893e4eb1f228c092a78ce9e6e676b34d3c
Naren <naren@happysanztech.com>
2020-12-28 17:59:03 +0530

    Commit naren

--- app/src/main/java/com/hst/osa/activity/MainActivity.java
+++ app/src/main/java/com/hst/osa/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.hst.osa;
+package com.hst.osa.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.hst.osa.R;
 
 public class MainActivity extends AppCompatActivity {
 
