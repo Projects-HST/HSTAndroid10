074973ee8947f1330383cd8ba321ef6b124e28c2
MineshDurai <minesh@happysanztech.com>
2020-12-31 11:53:32 +0530

    Minesh Committ

--- app/src/main/java/com/hst/osa/activity/SplashscreenActivity.java
+++ app/src/main/java/com/hst/osa/activity/SplashscreenActivity.java
@@ -1,16 +1,16 @@
 package com.hst.osa.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hst.osa.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
     }
 }
