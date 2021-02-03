48e2aed44661884b27062c79dcf58d57555f5c31
MineshDurai <minesh@happysanztech.com>
2021-02-03 10:37:49 +0530

    Minesh Committ

--- app/src/main/java/com/hst/osa/activity/LoginActivity.java
+++ app/src/main/java/com/hst/osa/activity/LoginActivity.java
@@ -2,25 +2,59 @@ package com.hst.osa.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.hst.osa.R;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = LoginActivity.class.getName();
+    private TextView btnFacebook;
+    private CallbackManager callbackManager;
+    private LoginManager loginManager;
+    private String url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.activity_toolbar);
+//        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_left_arrow));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -29,6 +63,27 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btnFacebook = findViewById(R.id.btn_fb_login);
+        btnFacebook.setOnClickListener(this);
+
+        try {
+            PackageInfo info = getApplicationContext().getPackageManager().getPackageInfo(
+                    "com.hst.osa",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        callbackManager = CallbackManager.Factory.create();
+
+        facebookLogin();
     }
 
     @Override
@@ -52,8 +107,88 @@ public class LoginActivity extends AppCompatActivity {
 
     public static void hideKeyboard(Activity activity) {
         if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
-            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
         }
     }
+
+    @Override
+    public void onClick(View v) {
+        loginManager.logInWithReadPermissions(
+                LoginActivity.this,
+                Arrays.asList("email", "public_profile"));
+    }
+
+    public void facebookLogin() {
+
+        loginManager = LoginManager.getInstance();
+//        callbackManager = CallbackManager.Factory.create();
+
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                    try {
+                        if (object != null) {
+                            String name = object.getString("name");
+                            String email = object.getString("email");
+                            String fbUserID = object.getString("id");
+
+                            Log.d(TAG, "name" + name + "email_id" + email);
+
+                            url = "https://graph.facebook.com/" + fbUserID + "/picture?type=large";
+//                            disconnectFromFacebook();
+                            // do action after Facebook login success
+                            // or call your API
+                            }
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+            @Override
+            public void onCancel() {
+                Log.v("LoginScreen", "---onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                // here write code when get error
+                Log.v("LoginScreen", "----onError: " + error.getMessage());
+            }
+        });
+    }
+
+    public void disconnectFromFacebook() {
+        if (AccessToken.getCurrentAccessToken() == null) {
+            return; // already logged out
+        }
+        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/",
+                null, HttpMethod.DELETE, new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse graphResponse) {
+                        LoginManager.getInstance().logOut();
+                    }
+                })
+                .executeAsync();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // add this line
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
