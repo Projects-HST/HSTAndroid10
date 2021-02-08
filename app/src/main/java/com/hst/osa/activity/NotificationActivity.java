58317caafa9b86ae9f0da588c6e6529e8e5c92a5
MineshDurai <minesh@happysanztech.com>
2021-02-08 15:15:50 +0530

    Minesh Committ

---
+++ app/src/main/java/com/hst/osa/activity/NotificationActivity.java
@@ -0,0 +1,16 @@
+package com.hst.osa.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hst.osa.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
